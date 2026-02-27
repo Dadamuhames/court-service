6dfab8bb1bda3fd87452d13c66bedccc9e721312
Dadamuhames <msd2007msd02@gmail.com>
2026-02-27 21:57:20 +0500

    fix: kafka config, case number generation

--- src/main/java/com/uzumtech/court/utils/CourtCaseNumberUtils.java
+++ src/main/java/com/uzumtech/court/utils/CourtCaseNumberUtils.java
@@ -1,26 +1,22 @@
 package com.uzumtech.court.utils;
 
-import com.uzumtech.court.repository.OffenseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
 import java.time.LocalDate;
-import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Component
 @RequiredArgsConstructor
 public class CourtCaseNumberUtils {
-    private final OffenseRepository legalOffenceRepository;
 
     public String generateProtocolNumber() {
         var today = LocalDate.now();
 
-        int countTodayOffenses = legalOffenceRepository.countByCreatedAt(today);
-
         String datePart = today.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 
-        String sequence = String.format("%05d", countTodayOffenses + 1);
+        String sequence = String.valueOf(Instant.now().toEpochMilli());
 
         return "JC-" + datePart + "-" + sequence;
     }
